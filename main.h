cac99c80cba71883faf7a4682c30f86c20deac93
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-24 01:19:47 -0800

    our header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
