9f977ebda739eb06d01453156be5f28ca8425572
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-24 04:11:22 -0800

    updating main.c

--- main.c
+++ main.c
@@ -1,65 +1,80 @@
 #include "main.h"
-
-
-char **environ; // extern char **environ;  // Ensure environ is declared properly
-
-// Define your copy function or remove the reference if not needed
-int copyContents() {
-    // Implement your copy function logic here
+char **environ;
+/**
+ * is_absolute_path - Check if a string is an absolute path.
+ * @str: The string to check.
+ * @argv: The argument vector.
+ * Return: 0 if not an absolute path, 1 otherwise.
+ */
+int is_absolute_path(char *str, char *argv[])
+{
+    if (str[0] == '/')
+        execve(str, argv, environ);
     return 0;
 }
 
-int executeCommand(char *command, char *arguments[]) {
-    // Execute the command using execve
-    if (execve(command, arguments, environ) == -1) {
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * main - Simple shell program.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv, char **envp)
+{
+    char *buffer = NULL, *token = NULL, *exit_command = "exit";
+    int buffer_size = 1, child_pid = 0, status = 0;
+    char *arguments[] = {"", NULL};
+    size_t line_length = 33;
 
-    return 0;
-}
-
-int main() {
-    char *inputLine = NULL;
-    size_t lineLength = 0;
-    ssize_t bytesRead;
-    pid_t childPid;
-    int childStatus;
-    char *commandArgs[] = { NULL, NULL }; // Initialize the array properly
+    (void)argc;
+    (void)argv;
+    (void)envp;
 
     while (1)
-      {
-        bytesRead = getline(&inputLine, &lineLength, stdin);
-        if (bytesRead == -1) {
-            free(inputLine);
-            exit(EXIT_SUCCESS);
+    {
+        buffer_size = getline(&buffer, &line_length, stdin);
+        token = strtok(buffer, " \n");
+
+        if (buffer_size == -1 || *buffer == *exit_command)
+        {
+            free(buffer);
+            exit(0);
+        }
+        else if (buffer == NULL)
+        {
+            free(buffer);
         }
 
-        // Tokenize the input
-        char *delimiter = " \n";
-        char *token = strtok(inputLine, delimiter);
+        child_pid = fork();
 
-        // Check for a special case (e.g., if the command starts with '.')
-        if (token != NULL && token[0] == '.') {
-            copyContents();
-            commandArgs[0] = "./hbtn_ls";
-            commandArgs[1] = "/var";
-            executeCommand(commandArgs[0], commandArgs);
-        } else {
-            // Execute the command and arguments
-            while (token != NULL) {
-                commandArgs[0] = token;
-                executeCommand(token, commandArgs);
-                token = strtok(NULL, delimiter);
+        if (child_pid == -1)
+        {
+            printf("Process error!\n");
+        }
+
+        if (child_pid == 0)
+        {
+            if (buffer[0] == '.')
+            {
+                copy_func();
+                arguments[0] = "./hbtn_ls";
+                arguments[1] = "/var";
+                arguments[2] = NULL;
+                execve("./hbtn_ls", arguments, environ);
+            }
+            else
+            {
+                token = strtok(buffer, " \n");
+                while (token != NULL)
+                {
+                    is_absolute_path(token, arguments);
+                    token = strtok(NULL, " \n");
+                }
             }
         }
-
-        // Wait for the child process to complete
-        wait(&childStatus);
+        else
+        {
+            wait(&status);
+        }
     }
-
-    // Free allocated memory for getline
-    free(inputLine);
 
     return 0;
 }
