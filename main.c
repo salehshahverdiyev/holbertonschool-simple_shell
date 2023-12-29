bb026a3d5c1ee5f53969a82d1fd96a0116542513
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-28 23:19:27 -0800

    updating function

--- main.c
+++ main.c
@@ -1,80 +1,42 @@
 #include "main.h"
-char **environ;
 /**
- * is_absolute_path - Check if a string is an absolute path.
- * @str: The string to check.
- * @argv: The argument vector.
- * Return: 0 if not an absolute path, 1 otherwise.
- */
-int is_absolute_path(char *str, char *argv[])
+ * main - entry
+ * Return: IDK
+*/
+int main(void)
 {
-    if (str[0] == '/')
-        execve(str, argv, environ);
-    return 0;
-}
+	char *buffer;
+	size_t bufsize = BUFFER_SIZE;
 
-/**
- * main - Simple shell program.
- * Return: Always 0.
- */
-int main(int argc, char **argv, char **envp)
-{
-    char *buffer = NULL, *token = NULL, *exit_command = "exit";
-    int buffer_size = 1, child_pid = 0, status = 0;
-    char *arguments[] = {"", NULL};
-    size_t line_length = 33;
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+	while (1)
+	{
+		printf("($) ");
+		getline(&buffer, &bufsize, stdin);
+		buffer[strlen(buffer) - 1] = '\0';
+		if (fork() == 0)
+		{
+			char *args[2];
 
-    (void)argc;
-    (void)argv;
-    (void)envp;
+			args[0] = buffer;
+			args[1] = NULL;
 
-    while (1)
-    {
-        buffer_size = getline(&buffer, &line_length, stdin);
-        token = strtok(buffer, " \n");
-
-        if (buffer_size == -1 || *buffer == *exit_command)
-        {
-            free(buffer);
-            exit(0);
-        }
-        else if (buffer == NULL)
-        {
-            free(buffer);
-        }
-
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            printf("Process error!\n");
-        }
-
-        if (child_pid == 0)
-        {
-            if (buffer[0] == '.')
-            {
-                copy_func();
-                arguments[0] = "./hbtn_ls";
-                arguments[1] = "/var";
-                arguments[2] = NULL;
-                execve("./hbtn_ls", arguments, environ);
-            }
-            else
-            {
-                token = strtok(buffer, " \n");
-                while (token != NULL)
-                {
-                    is_absolute_path(token, arguments);
-                    token = strtok(NULL, " \n");
-                }
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-
-    return 0;
+			if (execve(buffer, args, NULL) == -1)
+			{
+				perror(buffer);
+			}
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	free(buffer);
+	return (0);
 }
