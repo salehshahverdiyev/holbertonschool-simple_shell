cf4951d760aceb09bde122a542d12cd55a9556da
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-29 12:47:50 -0800

    trying different options

--- main.c
+++ main.c
@@ -13,81 +13,52 @@ void print_environment(void)
 		i++;
 	}
 }
-
-/**
- * execute_command - execute the given command
- * @buffer: command to be executed
- * @status: exit status
-*/
-void execute_command(char *buffer, int *status)
-{
-	if (strcmp(buffer, "exit") == 0)
-	{
-		free(buffer);
-		exit(*status);
-	}
-	else if (strcmp(buffer, "env") == 0)
-	{
-		print_environment();
-	}
-	else
-	{
-		if (fork() == 0)
-		{
-			char *token;
-			char *args[BUFFER_SIZE];
-			int i = 0;
-			
-			token = strtok(buffer, " ");
-			while (token != NULL)
-			{
-				args[i++] = token;
-				token = strtok(NULL, " ");
-			}
-			args[i] = NULL;
-			execvp(args[0], args);
-			perror(args[0]);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(status);
-			if (WIFEXITED(*status))
-			{
-				*status = WEXITSTATUS(*status);
-			}
-		}
-	}
-}
-
-/**
- * main - entry point
+/** main- entry point
  * Return: 0
- */
+*/
 int main(void)
 {
-	char *buffer;
-	size_t bufsize = BUFFER_SIZE;
+	char *buffer = NULL;
+	size_t bufsize = 0;
 	int status = 0;
 	
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(EXIT_FAILURE);
-	}
 	while (1)
 	{
 		printf("($) ");
 		getline(&buffer, &bufsize, stdin);
-		buffer[strlen(buffer) - 1] = '\0';
-		if (strcmp(buffer, "env") == 0)
+		buffer[strcspn(buffer, "\n")] = '\0';
+		if (strcmp(buffer, "exit") == 0)
+		{
+			free(buffer);
+			exit(status);
+		}
+		else if (strcmp(buffer, "env") == 0)
 		{
 			print_environment();
 		}
 		else
 		{
-			execute_command(buffer, &status);
+			if (fork() == 0)
+			{
+				char *token;
+				char *args[BUFFER_SIZE];
+				int i = 0;
+				
+				token = strtok(buffer, " ");
+				while (token != NULL)
+				{
+					args[i++] = token;
+					token = strtok(NULL, " ");
+				}
+				args[i] = NULL;
+				execvp(args[0], args);
+				perror(args[0]);
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				wait(&status);
+			}
 		}
 	}
 	free(buffer);
