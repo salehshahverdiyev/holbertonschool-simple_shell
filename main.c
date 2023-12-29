e4e119b9c084cc54ed1764e84837d5bdd29eaaaa
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-29 12:17:51 -0800

    fixing error cases => I want to sleep

--- main.c
+++ main.c
@@ -15,7 +15,53 @@ void print_environment(void)
 }
 
 /**
- * main - entry
+ * execute_command - execute the given command
+ * @buffer: command to be executed
+ * @status: exit status
+*/
+void execute_command(char *buffer, int *status)
+{
+	if (strcmp(buffer, "exit") == 0)
+	{
+		free(buffer);
+		exit(*status);
+	}
+	else if (strcmp(buffer, "env") == 0)
+	{
+		print_environment();
+	}
+	else
+	{
+		if (fork() == 0)
+		{
+			char *token;
+			char *args[BUFFER_SIZE];
+			int i = 0;
+			
+			token = strtok(buffer, " ");
+			while (token != NULL)
+			{
+				args[i++] = token;
+				token = strtok(NULL, " ");
+			}
+			args[i] = NULL;
+			execvp(args[0], args);
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(status);
+			if (WIFEXITED(*status))
+			{
+				*status = WEXITSTATUS(*status);
+			}
+		}
+	}
+}
+
+/**
+ * main - entry point
  * Return: 0
  */
 int main(void)
@@ -35,36 +81,7 @@ int main(void)
 		printf("($) ");
 		getline(&buffer, &bufsize, stdin);
 		buffer[strlen(buffer) - 1] = '\0';
-		if (strcmp(buffer, "exit") == 0)
-		{
-			free(buffer);
-			exit(status);
-		}
-		if (fork() == 0)
-		{
-			char *token;
-			char *args[BUFFER_SIZE];
-			int i = 0;
-				
-			token = strtok(buffer, " ");
-			while (token != NULL)
-			{
-				args[i++] = token;
-				token = strtok(NULL, " ");
-			}
-			args[i] = NULL;
-			execvp(args[0], args);
-			perror(args[0]);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status))
-			{
-				status = WEXITSTATUS(status);
-			}
-		}
+		execute_command(buffer, &status);
 	}
 	free(buffer);
 	return (0);
