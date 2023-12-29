c2b7447f218a2a96ccfb2c7fc2b5ede362625bd7
aelitaa <gunel.sultanova@outlook.com>
2023-12-29 03:17:32 -0800

    shell

--- main.c
+++ main.c
@@ -1,30 +1,52 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
  * main - entry
  * Return: IDK
-*/
+ */
 int main(void)
 {
 	char *buffer;
 	size_t bufsize = BUFFER_SIZE;
 
+	pid_t child_pid;
+	int status;
+	int use_absolute_path = 0;
+
 	buffer = (char *)malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 	{
 		perror("Unable to allocate buffer");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
+
 	while (1)
 	{
 		printf("($) ");
 		getline(&buffer, &bufsize, stdin);
 		buffer[strlen(buffer) - 1] = '\0';
+
 		if (strcmp(buffer, "exit") == 0)
 		{
 			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
-		if (fork() == 0)
+
+		if (buffer[0] == '/')
+		{
+			use_absolute_path = 1;
+		}
+
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("Error forking");
+			exit(EXIT_FAILURE);
+		}
+
+		if (child_pid == 0)
 		{
 			char *token;
 			char *args[BUFFER_SIZE];
@@ -37,16 +59,32 @@ int main(void)
 				token = strtok(NULL, " ");
 			}
 			args[i] = NULL;
-			if (args[0][0] != '/')
+
+			if (!use_absolute_path)
 			{
 				char full_path[BUFFER_SIZE];
 
-				snprintf(full_path, sizeof(full_path), "/bin/%s", args[0]);
-				if (execvp(full_path, args) == -1)
+				char *path = getenv("PATH");
+				if (path == NULL)
 				{
-					perror(full_path);
+					perror("Error getting PATH");
 					exit(EXIT_FAILURE);
 				}
+
+				token = strtok(path, ":");
+				while (token != NULL)
+				{
+					snprintf(full_path, sizeof(full_path), "%s/%s", token, args[0]);
+					if (execvp(full_path, args) != -1)
+					{
+						exit(EXIT_SUCCESS);
+					}
+
+					token = strtok(NULL, ":");
+				}
+
+				perror(args[0]);
+				exit(EXIT_FAILURE);
 			}
 			else
 			{
@@ -59,9 +97,10 @@ int main(void)
 		}
 		else
 		{
-			wait(NULL);
+			waitpid(child_pid, &status, 0);
 		}
 	}
+
 	free(buffer);
-	return (0);
+	return 0;
 }
